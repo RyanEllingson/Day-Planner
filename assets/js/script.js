171afc69ee345c919c4e0f8b409f8c67ba0f439a
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-02-02 21:18:41 -0600

    colors now update depending on the current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 function initPage() {
     const currentHour = parseInt(moment().format("kk"));
-    console.log(currentHour);
     const currentDayEl = document.getElementById("currentDay");
     currentDayEl.innerHTML = moment().format("dddd, MMMM Do YYYY");
 
@@ -47,36 +46,6 @@ function initPage() {
     const timeblockContainer = document.getElementById("timeblock-container");
     timeblockContainer.innerHTML = "";
 
-    // const inputEl9AM = document.getElementById("9AMinput");
-    // const inputEl10AM = document.getElementById("10AMinput");
-    // const inputEl11AM = document.getElementById("11AMinput");
-    // const inputEl12PM = document.getElementById("12PMinput");
-    // const inputEl1PM = document.getElementById("1PMinput");
-    // const inputEl2PM = document.getElementById("2PMinput");
-    // const inputEl3PM = document.getElementById("3PMinput");
-    // const inputEl4PM = document.getElementById("4PMinput");
-    // const inputEl5PM = document.getElementById("5PMinput");
-
-    // const saveEl9AM = document.getElementById("9AMsave");
-    // const saveEl10AM = document.getElementById("10AMsave");
-    // const saveEl11AM = document.getElementById("11AMsave");
-    // const saveEl12PM = document.getElementById("12PMsave");
-    // const saveEl1PM = document.getElementById("1PMsave");
-    // const saveEl2PM = document.getElementById("2PMsave");
-    // const saveEl3PM = document.getElementById("3PMsave");
-    // const saveEl4PM = document.getElementById("4PMsave");
-    // const saveEl5PM = document.getElementById("5PMsave");
-
-    // <div class="row input-group time-block">
-    //     <div class="input-group-prepend">
-    //       <span class="input-group-text hour">2 PM</span>
-    //     </div>
-    //     <input type="text" class="form-control description" id="2PMinput">
-    //     <div class="input-group-append">
-    //       <span class="saveBtn" id="2PMsave"><i class="fas fa-save"></i></span>
-    //     </div>
-    //   </div>
-
     for (let i=0; i<dayPlan.length; i++) {
         let hourString;
         const hour = 9 + i;
@@ -102,7 +71,13 @@ function initPage() {
 
         const noteEl = document.createElement("input");
         noteEl.setAttribute("type", "text");
-        noteEl.setAttribute("class", "form-control description");
+        if (hour < currentHour) {
+            noteEl.setAttribute("class", "form-control description past");
+        } else if (hour === currentHour) {
+            noteEl.setAttribute("class", "form-control description present");
+        } else {
+            noteEl.setAttribute("class", "form-control description future");
+        }
         noteEl.setAttribute("placeholder", dayPlan[i].note);
         timeBlock.append(noteEl);
 
@@ -114,13 +89,10 @@ function initPage() {
         saveSpan.setAttribute("class", "saveBtn");
         saveContainer.append(saveSpan);
 
-        // localStorage.setItem("search",JSON.stringify(searchHistory));
-
         const saveIcon = document.createElement("i");
         saveIcon.setAttribute("class", "fas fa-save");
         saveIcon.addEventListener("click", () => {
             dayPlan[i].note = noteEl.value;
-            console.log(dayPlan);
             localStorage.setItem("plan", JSON.stringify(dayPlan));
         });
         saveSpan.append(saveIcon);
